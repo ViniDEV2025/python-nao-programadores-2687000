6a5e214dcf54e6afd58a5519a0e66c15f5c9aa93
ViniDEV2025 <viniciuswagner2023x@gmail.com>
2026-06-24 04:33:03 +0000

    1-variaveis.py exercicios/1-primeiros-passos/1-variaveis.pyexercicios

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -7,5 +7,43 @@
 
 # Declare variáveis de cada tipo numérico
 
-
 # Declare uma variável do tipo booleano
+
+
+print('meu nome é Wagner!')
+print('31 anos!')
+print('eu sou estudante de analise e desenvolvimento de sistemas!')
+
+idade= 25
+pode_dirigir = idade >= 18
+print(pode_dirigir)
+
+tem_estoque = True
+
+if  tem_estoque:
+    print("Produto disponivel")
+
+    #string
+
+    nome= "wagner"
+    curso= "ads"
+
+    #númerico
+
+    #int
+    idade= 31
+    quantidade_produtos = 100
+    ano= 2026
+
+    print(idade)
+    print(quantidade_produtos)
+    print(ano)
+
+
+#float
+
+altura= 1.76
+preco= 7.50
+
+print (altura)
+print (preco)
